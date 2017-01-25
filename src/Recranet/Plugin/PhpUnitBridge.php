3761ad4cb462c39d6a62447a4ff72439b9147a48
Raymon de Looff <raydelooff@gmail.com>
2017-01-25 11:19:38 +0100

    Do not extend the PhpUnit plugin

--- src/Recranet/Plugin/PhpUnitBridge.php
+++ src/Recranet/Plugin/PhpUnitBridge.php
@@ -9,88 +9,115 @@
 
 namespace Recranet\Plugin;
 
-use PHPCI\Plugin\PhpUnit as PhpUnitPlugin;
+use PHPCI\Plugin;
+use PHPCI\Builder;
+use PHPCI\Model\Build;
+use PHPCI\ZeroConfigPlugin;
 
 /**
 * PHPUnit Bridge Plugin - Allows PHPUnit testing with the Symfony PHPUnit Bridge.
 *
 * @author Raymon de Looff <raydelooff@gmail.com>
 */
-class PhpUnitBridge extends PhpUnitPlugin
+class PhpUnitBridge implements Plugin, ZeroConfigPlugin
 {
+    /**
+     * @var \PHPCI\Builder
+     */
+    protected $phpci;
+
+    /**
+     * @var \PHPCI\Model\Build
+     */
+    protected $build;
+
+    /**
+     * @var string
+     */
+    protected $workingDirectory;
+
+    /**
+     * @var string
+     */
+    protected $options;
+
+    /**
+     * @var string
+     */
+    protected $path;
+
+    /**
+     * Constructor.
+     *
+     * @param Builder $phpci
+     * @param Build $build
+     * @param array $options
+     */
+    public function __construct(Builder $phpci, Build $build, array $options = array())
+    {
+        $this->phpci = $phpci;
+        $this->build = $build;
+
+        $this->parseOptions($options);
+    }
+
+    /**
+     * Parse the options.
+     *
+     * @param $options
+     */
+    public function parseOptions($options)
+    {
+        if (array_key_exists('working_directory', $options)) {
+            $this->workingDirectory = $options['working_directory'];
+        }
+
+        if (array_key_exists('options', $options)) {
+            if (!is_array($options['options'])) {
+                $options['options'] = array($options['options']);
+            }
+
+            $this->options = implode(' ', $options['options']);
+        }
+
+        if (array_key_exists('path', $options)) {
+            $this->path = $options['path'];
+        }
+    }
+
+    /**
+     * Check if the plugin can be executed without any configurations
+     *
+     * @param $stage
+     * @param Builder $builder
+     * @param Build $build
+     *
+     * @return bool
+     */
+    public static function canExecute($stage, Builder $builder, Build $build)
+    {
+        return $stage == 'test';
+    }
+
     /**
     * Runs PHPUnit tests in a specified directory, optionally using specified config file(s).
     */
     public function execute()
     {
-        if (empty($this->xmlConfigFile) && empty($this->directory)) {
-            $this->phpci->logFailure('Neither configuration file nor test directory found.');
-
-            return false;
+        if ($this->workingDirectory) {
+            $currentDirectory = getcwd();
+            chdir($this->phpci->buildPath . DIRECTORY_SEPARATOR . $this->workingDirectory);
         }
 
-        // Run any config files first. This can be either a single value or an array.
-        if ($this->xmlConfigFile) {
-            return $this->runConfigFile($this->xmlConfigFile);
+        $phpunit = $this->phpci->findBinary('simple-phpunit');
+
+        $cmd = $phpunit . ' %s %s';
+        $success = $this->phpci->executeCommand($cmd, $this->options, $this->path);
+
+        if ($this->workingDirectory) {
+            chdir($currentDirectory);
         }
 
-        // Run any dirs next. Again this can be either a single value or an array.
-        if ($this->directory) {
-            return $this->runDir($this->directory);
-        }
-
-        return true;
-    }
-
-    /**
-     * Run the tests defined in a PHPUnit config file.
-     *
-     * @param $configPath
-     * @return bool|mixed
-     */
-    protected function runConfigFile($configPath)
-    {
-        if (is_array($configPath)) {
-            return $this->recurseArg($configPath, array($this, 'runConfigFile'));
-        } else {
-            if ($this->runFrom) {
-                $curdir = getcwd();
-                chdir($this->phpci->buildPath . DIRECTORY_SEPARATOR . $this->runFrom);
-            }
-
-            $phpunit = $this->phpci->findBinary('simple-phpunit');
-
-            $cmd = $phpunit . ' --tap %s -c "%s" ' . $this->coverage . $this->path;
-            $success = $this->phpci->executeCommand($cmd, $this->args, $this->phpci->buildPath . $configPath);
-
-            if ($this->runFrom) {
-                chdir($curdir);
-            }
-
-            return $success;
-        }
-    }
-
-    /**
-     * Run the PHPUnit tests in a specific directory or array of directories.
-     *
-     * @param $directory
-     * @return bool|mixed
-     */
-    protected function runDir($directory)
-    {
-        if (is_array($directory)) {
-            return $this->recurseArg($directory, array($this, 'runDir'));
-        } else {
-            $curdir = getcwd();
-            chdir($this->phpci->buildPath);
-
-            $phpunit = $this->phpci->findBinary('simple-phpunit');
-
-            $cmd = $phpunit . ' --tap %s "%s"';
-            $success = $this->phpci->executeCommand($cmd, $this->args, $this->phpci->buildPath . $directory);
-            chdir($curdir);
-            return $success;
-        }
+        return $success;
     }
 }
